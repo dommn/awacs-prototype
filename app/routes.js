8c00251660aa213d319ef96deafb268ea7e4a282
dommn <47447454+dommn@users.noreply.github.com>
2021-08-18 11:12:49 +0100

    Revert "Initial upload"
    This reverts commit 293cc52854fdf260d360485fa520bf24a55767b0.

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Validation routing depending on sort code that's entered
-router.use('/01', require('./views/01/_routes'));
-router.use('/02', require('./views/02/_routes'));
-router.use('/warbreck-testing-august', require('./views/warbreck-testing-august/_routes'));
-
-module.exports = router
