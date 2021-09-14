2b9625acb53de2da36ce31f4e2cfcbbd63b1ab83
dommn <47447454+dommn@users.noreply.github.com>
2021-09-14 12:07:23 +0100

    Added admin prototype

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,7 @@ const router = express.Router()
 router.use('/01', require('./views/01/_routes'));
 router.use('/02', require('./views/02/_routes'));
 router.use('/warbreck-testing-august', require('./views/warbreck-testing-august/_routes'));
+router.use('/admin/01', require('./views/admin/01/_routes'));
+
 
 module.exports = router
