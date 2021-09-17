0fa64d1273a77877803e622ed3744c27946a0563
dommn <47447454+dommn@users.noreply.github.com>
2021-09-17 11:00:57 +0100

    Updated nav links

--- app/views/admin/01/_routes.js
+++ app/views/admin/01/_routes.js
@@ -3,45 +3,45 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/management-information/search-month', function (req, res) {
+router.post('/search-month', function (req, res) {
 
   let searchMonth = req.session.data['search-date-month']
 
   if (searchMonth === '01') {
-    res.redirect('january-2021')
+    res.redirect('management-information/january-2021')
   }
 
   else if (searchMonth === '02') {
-    res.redirect('february-2021')
+    res.redirect('management-information/february-2021')
   }
 
   else if (searchMonth === '03') {
-    res.redirect('march-2021')
+    res.redirect('management-information/march-2021')
   }
 
   else if (searchMonth === '04') {
-    res.redirect('april-2021')
+    res.redirect('management-information/april-2021')
   }
 
   else if (searchMonth === '05') {
-    res.redirect('may-2021')
+    res.redirect('management-information/may-2021')
   }
 
   else if (searchMonth === '06') {
-    res.redirect('june-2021')
+    res.redirect('management-information/june-2021')
   }
 
   else if (searchMonth === '07') {
-    res.redirect('july-2021')
+    res.redirect('management-information/july-2021')
   }
 
   else if (searchMonth === '08') {
-    res.redirect('august-2021')
+      res.redirect('management-information/august-2021')
   }
 
 
   else if (searchMonth === '09') {
-    res.redirect('september-2021')
+    res.redirect('management-information/september-2021')
   }
 
 })
