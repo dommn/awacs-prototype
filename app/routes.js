1ddb3ba13ad02f568440d7935d60abba48ff3ae2
dommn <47447454+dommn@users.noreply.github.com>
2021-10-01 12:03:26 +0100

    Added MI 2.0

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,7 @@ router.use('/01', require('./views/01/_routes'));
 router.use('/02', require('./views/02/_routes'));
 router.use('/warbreck-testing-august', require('./views/warbreck-testing-august/_routes'));
 router.use('/admin/01', require('./views/admin/01/_routes'));
+router.use('/admin/02', require('./views/admin/02/_routes'));
 
 
 module.exports = router
