df048b4068b9d487bef388612ba80267d8b92b50
dommn <47447454+dommn@users.noreply.github.com>
2021-10-05 20:52:20 +0100

    Added updated version of prototype for UR sessions

--- app/views/admin/02/_routes.js
+++ app/views/admin/02/_routes.js
@@ -11,39 +11,6 @@ router.post('/management-information/search-month', function (req, res) {
     res.redirect('january-2021')
   }
 
-  else if (searchMonth === '02' || searchMonth === '2') {
-    res.redirect('february-2021')
-  }
-
-  else if (searchMonth === '03' || searchMonth === '3') {
-    res.redirect('march-2021')
-  }
-
-  else if (searchMonth === '04' || searchMonth === '4') {
-    res.redirect('april-2021')
-  }
-
-  else if (searchMonth === '05' || searchMonth === '5') {
-    res.redirect('may-2021')
-  }
-
-  else if (searchMonth === '06' || searchMonth === '6') {
-    res.redirect('june-2021')
-  }
-
-  else if (searchMonth === '07' || searchMonth === '7') {
-    res.redirect('july-2021')
-  }
-
-  else if (searchMonth === '08' || searchMonth === '8') {
-      res.redirect('august-2021')
-  }
-
-
-  else if (searchMonth === '09' || searchMonth === '9') {
-    res.redirect('september-2021')
-  }
-
 })
 
 router.post('/users/search-month', function (req, res) {
